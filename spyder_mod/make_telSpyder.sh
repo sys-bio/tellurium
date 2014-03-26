4a857940f085e458aff5bb189d7683820b025621
Michal Galdzicki <mgaldzic@gmail.com>
2014-03-26 01:02:32 -0700

    already in cwd path corected

--- spyder_mod/make_telSpyder.sh
+++ spyder_mod/make_telSpyder.sh
@@ -1,5 +1,5 @@
 cp -Rv spyderlib/* ../installer/windows/spyder/spyderlib/
 cp -Rv scripts/* ../installer/windows/spyder/scripts/
 cd ../installer/windows/spyder
-python setup.py bdist --formats=wininst --dist-dir ../tellurium/installer/windows/spyder_dependencies
+python setup.py bdist --formats=wininst --dist-dir ../spyder_dependencies
 
