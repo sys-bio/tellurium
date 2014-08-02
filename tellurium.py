f66de015d247ddb297be43caa79ecb8ca1b330f1
Herbert Sauro <hsauro@u.washington.edu>
2014-08-01 18:36:26 -0700

    Many updates to docs, addition of listTestModels()

--- tellurium.py
+++ tellurium.py
@@ -4,7 +4,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 10 14:34:07 2013
-Updated: August 1, 2014
+Updated: July 30, 2014
 
 @author: Herbert M Sauro
 
@@ -18,6 +18,8 @@ import roadrunner.testing
 import libantimony
 import tellurium
 import numpy
+import os
+import glob
 
 try:
     from sbml2matlab import sbml2matlab
@@ -29,6 +31,11 @@ except ImportError as e:
 #@{
 
 ##\brief Returns the version number for Tellurium
+#
+##\brief Set the seed used by the internal random number generator
+#
+#Example: print te.getTelluriumVersion()
+#
 #\return Returns a string representing the version number
 def getTelluriumVersion():
     import os
@@ -44,6 +51,11 @@ def getTelluriumVersion():
 #@{
 
 ##\brief Save a string to a file
+#
+#Example: saveToFile ('c:\\myfile.txt', strVariable)
+#
+#\param[in] fileName The current time in the simulation
+#\param[in] str The step size to use in the integration
 #\return Saves a string to a file
 def saveToFile (fileName, str):
     """Save a string to a file. Takes two arguments, 
@@ -55,6 +67,10 @@ def saveToFile (fileName, str):
     outFile.close()
 
 ##\brief Reads a text file into a string
+#
+#Example: str = readFromFile ('c:\\myfile.txt')
+#
+#\param[in] fileName The current time in the simulation
 #\return Returns a string representing the contents of the text file
 def readFromFile (fileName):
     """Load a file and return contents as a string, 
@@ -71,12 +87,20 @@ def readFromFile (fileName):
 #@{
 
 ##\brief Load an SBML model into roadRunner
+#
+#Example: rr = loadSBMLModel ('c:\\myfile.txt')
+#
+#\param[in] sbml A filename or a string containing SBML
 #\return Returns a reference to the roadrunner model
 def loadSBMLModel (sbml):
     rr = roadrunner.RoadRunner (sbml)
     return rr
     
-##\brief Reads an antimony string into roadrunner
+##\brief Reads an Antimony string into roadrunner
+#
+#Example: rr = loadAntimonyModel (modelStr)
+#
+#\param[in] antStr A string containing a Antimony model
 #\return Returns a reference to the roadrunner model
 def loadAntimonyModel (antStr):
     """Load an Antimony string:
@@ -96,12 +120,16 @@ def loadAntimonyModel (antStr):
     
     return rr
 
-##\brief Reads an antimony string into roadrunner, short-cut to loadAntimonyModel()
+##\brief Reads an Antimony string into roadrunner, short-cut to loadAntimonyModel()
+#
+#Example: rr = loada ('S1 -> S2; k1*S1; k1 = 0.1; S2 = 10')
+#
+#\param[in] antStr A string containing a Antimony model
 #\return Returns a reference to the roadrunner model
-def loada (antstr):
-    return loadAntimonyModel (antstr)
+def loada (antStr):
+    return loadAntimonyModel (antStr)
    
-##\brief Reads a CellMl string into roadrunner
+##\brief Reads a CellML string into roadrunner
 #\return Returns a reference to the roadrunner model  
 def loadCellMLModel (cellML):
     import os
@@ -200,7 +228,13 @@ def cellmlStrToSBML (CellMLStr):
 # ---------------------------------------------------------------------
 ##\ingroup math
 #@{
-   
+ 
+##\brief Compute the eigenvalues for numpy array
+#
+#Example: mat = rr.getEigenvalues(matrix)
+#
+#\param[in] m numpy array
+#\return Returns a numpy array containing the eigenvalues
 def getEigenvalues (m):
     """
     Convenience method for computing the eigenvalues for a matrix, m
@@ -216,6 +250,11 @@ def getEigenvalues (m):
 ##\ingroup stochastic
 #@{
 
+##\brief Return the current seed used by the internal random number generator
+#
+#Example: myseed = rr.getSeed()
+#
+#\return The seed value
 def getSeed (r):
     """
     Return the current seed used by the random generator
@@ -225,6 +264,10 @@ def getSeed (r):
         raise ValueError("model is not loaded")
     return intg['seed']
         
+##\brief Set the seed used by the internal random number generator
+#
+#Example: rr.setSeed(12345)
+#
 def setSeed (r, seed):
     """
     Set the seed for the random number generator (used by gillespie for example)
@@ -237,6 +280,10 @@ def setSeed (r, seed):
     # to C integers on 64 bit machines. If its converted to float before, works around the issue. 
     intg['seed'] = float(seed)
 
+##\brief Run a Gillespie simulation
+#
+#Example: result = rr.gillespie (0, 100)
+#
 def gillespie (r, *args, **kwargs):
     """
     Run a Gillespie stochastic simulation. 
@@ -415,27 +462,6 @@ def gillespie (r, *args, **kwargs):
     return result
 ##@} 
 
-#def augmentRoadrunnerCtor():
-#    """Hides the need to use Antimony directly from user
-#    Overwrite the Roadrunner Constructor to accept Antimony string
-#    
-#    This is done at the begining of the tellurium startup
-#    """
-#    original_init = roadrunner.RoadRunner.__init__
-#
-#    def new_init(self, *args):
-#        #get sbml and recompose args tuple
-#        if (len(args) > 1 and libantimony.loadAntimonyString(args[0]) >= 0):
-#            args = ((antimonyTosbml(args[0]),) + args[1:])
-#        elif (len(args) == 1 and libantimony.loadAntimonyString(args[0]) >= 0):
-#            args = (antimonyTosbml(args[0]),)
-#        else:
-#            pass
-#            
-#        original_init(self, *args)
-#
-#    roadrunner.RoadRunner.__init__ = new_init
-
 def RoadRunner(args):
     return roadrunner.RoadRunner(args)
 
@@ -443,6 +469,7 @@ def RoadRunner(args):
 ##\ingroup plotting
 #@{
 
+#\cond
 def plotWithLegend (r, result):
     """
     Plot an array and include a legend. The first argument must be a roadrunner variable. 
@@ -469,7 +496,9 @@ def plotWithLegend (r, result):
         roadrunner.Config.setValue(rr.Config.SIMULATEOPTIONS_STRUCTURED_RESULT, False)
         to set the right mode."""       
         raise Exception (str)
-   
+#\endcond
+ 
+#\cond  
 def simulateAndPlot (rr, startTime=0, endTime=5, numberOfPoints=500):
     """
     Carry out a simulation and plot the results. Returns the result to the caller 
@@ -483,8 +512,13 @@ def simulateAndPlot (rr, startTime=0, endTime=5, numberOfPoints=500):
     result = rr.simulate (startTime, endTime, numberOfPoints)
     tellurium.plotWithLegend (rr, result)   
     return result
+#\endcond
     
-# Plot a numpy array
+##\brief Plot a numpy array where the first column is considered the x axis and all remaining columns the y axis.
+#
+#Example: te.plotArray (result)
+#\param[in] result Numpy Array
+#\return Returns the plot object
 def plotArray (result):
     """
     Plot an array. The first column of the array will
@@ -498,20 +532,45 @@ def plotArray (result):
     plt.show()
     return plt
 
-def plot (result):
-    return plotArray (result)
+##\brief Plot results from a simulation carried out by the simulate or gillespie functions. This is a roadrunner method.
+#
+# Plot data generated by a simulation:
+#
+#Example: rr.plot (result)
+#
+# Data is a numpy array where the first column is considered the x axis and all remaining columns the y axis.
+#
+# Plot data currently held by roadrunner that was generated in the last simulation.
+#
+#Example : rr.plot()
+#
+#\param[in] result Data returned from a simulate or gillespie call
+#\return Returns the plot object
+def plot (self, result=None):
+    if result is None:
+	   # Call Andy version if no results passed to call
+       return self.plotAS()
+    else:
+       return plotWithLegend (self, result)
 ##@} 
  
 # ---------------------------------------------------------------------
 ##\ingroup export
 #@{
-   
+
+##\brief Returns a Matlab function that represents the current model
+#
+#Example: print rr.getMatlab()
+#\return Returns a string representing the Matlab code
 def getMatlab (self):
     """
     Returns Matlab string for current model
     """
     return sbml2matlab(self.getCurrentSBML())
   
+##\brief Convenience method for exporting the current model as a Matlab function to a file
+#
+#Example: rr.exportToMatlab ('c:\\mymodel.m')
 def exportToMatlab (self, fileName):
     """
     Save the current model as a Matlab file to the give file name
@@ -522,18 +581,51 @@ def exportToMatlab (self, fileName):
     saveToFile (fileName, self.getMatlab())
 ##@} 
 
+# ---------------------------------------------------------------------
+##\ingroup testmodels
+#@{
+
+##\brief Loads a particular test model into roadrunner
+#
+#Example: rr = roadrunner.loadTestModel ('feedback.xml') 
+# 
+#\return Returns a reference to roadrunner
+def loadTestModel (str):
+    """
+    Loads the test model into roadrunner
+    """
+    return roadrunner.testing.getRoadRunner(str)
+
+##\brief Returns the SBML for a particular test model
+#
+#Example: sbmlStr = roadrunner.getTestModel ('feedback.xml') 
+# 
+#\return Returns a string containing the test model in SBML format
 def getTestModel (str):
     """
     Returns the model as a string from the test directory
     """
     return roadrunner.testing.getData (str)
     
-def loadTestModel(str):
-    """
-    Loads the test model into roadrunner and returns a roadrunner variable
-    """
-    return roadrunner.RoadRunner (getTestModel (str)) 
- 
+##\brief Returns the list of possible test models
+#
+#Example: print roadrunner.listTestModels() 
+# 
+#\return Returns the list of available test model names
+def listTestModels():
+    # Get the path where roadrunner is installed
+	  d = os.path.abspath(roadrunner.__file__)
+	  rpath = os.path.dirname (d)
+	  # The test files are located in testing
+	  rpath = rpath + '\\testing'
+	  tmp = os.getcwd()
+	  os.chdir (rpath)
+	  modelList = glob.glob ('*.xml')
+	  os.chdir (tmp)
+	  return modelList
+##@} 	
+	
+# ---------------------------------------------------------------------
 def noticesOff ():
     """
     Switch off the generation of notices to the user
@@ -560,12 +652,19 @@ def getRatesOfChange (self):
 # ---------------------------------------------------------------------
 ##\ingroup resetmethods
 #@{
+
+##\brief Reset the model back to the state it was when it was first loaded
+#
+#Example: rr.resetToOrigin ()
 def resetToOrigin(self):
     """ This resets the model back to the state is was when it 
     was FIRST loaded, this includes all init() and parameters such as k1 etc.
     """
     self.reset(SelectionRecord.ALL)
-   
+
+##\brief Reset all the state variables to the current initial condition values
+#
+#Example: rr.reset ()
 def reset(self):
     """
     This resets the variables, S1, S2 etc to the CURRENT init(X) values. 
@@ -573,6 +672,9 @@ def reset(self):
     """
     self.reset(SelectionRecord.TIME | SelectionRecord.RATE | SelectionRecord.FLOATING)
 
+##\brief Reset all the state variables to the current initial condition values and the global parameters back to when the model was first loaded.
+#
+#Example: rr.reset ()
 def resetAll (self):    
     """
     This resets all variables, S1, S2 etc to the CURRENT init(X) values. It also resets all
@@ -582,13 +684,17 @@ def resetAll (self):
     self.reset(SelectionRecord.TIME | SelectionRecord.RATE | SelectionRecord.FLOATING | SelectionRecord.PARAMETER)
 ##@} 
 
-# ---------------------------------------------------- 
+# --------------------------------------------------------------------- 
 # Routines to support the Jarnac compatibility layer
-
 # ---------------------------------------------------------------------
 ##\ingroup jarnac
 #@{
 
+##\brief Get the stoichiometry matrix for the current model
+#
+# Can be further shortened to rr.sm()
+#
+#Example: mat = rr.getSm()
 def getSm (self):
     """
     Returns the full reordered stoichiometry matrix.
@@ -598,7 +704,12 @@ def getSm (self):
     print rr.sm()
     """
     return self.getFullStoichiometryMatrix()
-    
+ 
+##\brief Get the names for the reactions in the current model
+#
+# Can be further shortened to rr.rs()
+#
+#Example: reactionNames = rr.getRs()  
 def getRs (self):
     """
     Returns the list of reaction Identifiers
@@ -609,6 +720,11 @@ def getRs (self):
     """
     return self.model.getReactionIds()
     
+##\brief Get the names for the floating species in the current model
+#
+# Can be further shortened to rr.fs()
+#
+#Example: floatingSpeciesNames = rr.getFs()  
 def getFs (self):
     """  
     Returns the list of floating species identifiers
@@ -619,6 +735,11 @@ def getFs (self):
     """
     return self.model.getFloatingSpeciesIds()
 
+##\brief Get the names for the boundary species in the current model
+#
+# Can be further shortened to rr.bs()
+#
+#Example: boundarySpeciesNames = rr.getBs()  
 def getBs (self):
     """  
     Returns the list of boundary species identifiers
@@ -630,6 +751,11 @@ def getBs (self):
     return self.model.getBoundarySpeciesIds()
 
 
+##\brief Get the names for the rglobal parameters in the model
+#
+# Can be further shortened to rr.ps()
+#
+#Example: parameterNames = rr.getPs()  
 def getPs (self):
     """  
     Returns the list of global parameters in the model
@@ -639,7 +765,12 @@ def getPs (self):
     print rr.ps()
     """
     return self.model.getGlobalParameterIds()
-    
+ 
+##\brief Get the names for the compartments in the current model
+#
+# Can be further shortened to rr.vs()
+#
+#Example: compartmentNames = rr.getVs()   
 def getVs (self):
     """  
     Returns the list of compartment identifiers
@@ -650,7 +781,12 @@ def getVs (self):
     """
     return self.model.getCompartmentIds()
     
-    
+  
+##\brief Get all the rates of change in the current model
+#
+# Can be further shortened to rr.dv()
+#
+#Example: rateOfChange = rr.getDv()  
 def getDv (self):
     """  
     Returns the list of rates of change
@@ -661,6 +797,11 @@ def getDv (self):
     """
     return self.model.getStateVectorRate()
     
+##\brief Get all the reaction rates in the current model
+#
+# Can be further shortened to rr.rv()
+#
+#Example: reactionRates = rr.getRv()  
 def getRv (self):
     """  
     Returns the list of reaction rates
@@ -671,6 +812,11 @@ def getRv (self):
     """
     return self.model.getReactionRates()
 
+##\brief Get all the floating species concentrations in the current model
+#
+
+#
+#Example: reactionRates = rr.getSv()  
 def getSv (self):
     """  
     Returns the list of flaoting species concentrations
@@ -689,9 +835,13 @@ roadrunner.RoadRunner.gillespie = gillespie
 roadrunner.RoadRunner.getRatesOfChange = getRatesOfChange
 roadrunner.RoadRunner.exportToMatlab = exportToMatlab
 roadrunner.RoadRunner.getMatlab = getMatlab
+roadrunner.RoadRunner.plotAS = roadrunner.RoadRunner.plot
+roadrunner.RoadRunner.plot = plot
 
 roadrunner.noticesOff = noticesOff
 roadrunner.noticesOn = noticesOn  
+roadrunner.loadTestModel = loadTestModel
+roadrunner.listTestModels = listTestModels
 
 roadrunner.RoadRunner.resetToOrigin = resetToOrigin
 roadrunner.RoadRunner.reset = reset
@@ -715,15 +865,35 @@ roadrunner.RoadRunner.sv = getSv
 #\section Introduction
 #Tellurium is a cross-platform and open source Python based environment that integrates a variety of useful packages for systems biology modeling. The IDE is based on the spyder2 IDE (https://code.google.com/p/spyderlib/)
 #
-#\par Example:
+#\par Simple Example:
 #\par
 #@code
 #
-#r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
-#r.simulate (0, 10, 100)
-#r.plot()
+#rr = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+#rr.simulate (0, 10, 100)
+#rr.plot()
+#
 #@endcode
 #
+#\par More Complex Example:
+#\par
+#@code
+#
+#import tellurium at te
+#
+#r = te.loada ('''
+#    # A dollar symbol means fix the species concentration
+#        $S1 -> S2;  k1*S1; 
+#     J2: S2 -> S3;  k2*S2 - k3*S3;
+#         S3 -> $S4; k4*S3;
+#    
+#    k1 = 0.1; S1 = 10
+#''')
+#
+#result = r.simulate (0, 10, 100, ['time', 'S1', 'S3', 'J1'])
+#rr.plot (result)
+#
+#@endcode
 #
 #\par Screen-shot of Tellurium
 #\par 
@@ -758,3 +928,29 @@ roadrunner.RoadRunner.sv = getSv
 
 ##\defgroup jarnac Short-cut methods
 # \brief Useful short-cut methods
+
+##\defgroup testmodels Test Models
+# \brief Methods to acess the builtin test models
+
+
+# OLD CODE----------------------------------------
+#def augmentRoadrunnerCtor():
+#    """Hides the need to use Antimony directly from user
+#    Overwrite the Roadrunner Constructor to accept Antimony string
+#    
+#    This is done at the begining of the tellurium startup
+#    """
+#    original_init = roadrunner.RoadRunner.__init__
+#
+#    def new_init(self, *args):
+#        #get sbml and recompose args tuple
+#        if (len(args) > 1 and libantimony.loadAntimonyString(args[0]) >= 0):
+#            args = ((antimonyTosbml(args[0]),) + args[1:])
+#        elif (len(args) == 1 and libantimony.loadAntimonyString(args[0]) >= 0):
+#            args = (antimonyTosbml(args[0]),)
+#        else:
+#            pass
+#            
+#        original_init(self, *args)
+#
+#    roadrunner.RoadRunner.__init__ = new_init
