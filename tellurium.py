2ed4a839ede42e9bc24717c01347c9fe3b324244
Herbert Sauro <hsauro@u.washington.edu>
2014-08-04 19:52:01 -0700

    Added lots of small examples to the docs

--- tellurium.py
+++ tellurium.py
@@ -4,7 +4,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 10 14:34:07 2013
-Updated: July 30, 2014
+Updated: August 3, 2014
 
 @author: Herbert M Sauro
 
@@ -711,6 +711,7 @@ roadrunner.RoadRunner.plot = plot
 
 roadrunner.noticesOff = noticesOff
 roadrunner.noticesOn = noticesOn  
+roadrunner.getTestModel = getTestModel
 roadrunner.loadTestModel = loadTestModel
 roadrunner.listTestModels = listTestModels
 
@@ -772,37 +773,239 @@ roadrunner.RoadRunner.sv = getSv
 #
 ##\defgroup utility Utility Methods
 # \brief Various utility methods
+#
+# The most useful methods here are the notices routines. Roadrunner will offen issue warning or informational messages. For
+# repeated simulation such messages will clutter up the outputs. noticesOff and noticesOn can be used to turn on an off the messages.
+#
+#Examples:<br>
+#@code
+#  # Repeat a simulation many times
+#
+#  # Load an SBML file
+#  r = roadrunner.RoadRunner ('mymodel.xml')
+#  # Turn of notices so they don't clutter the output
+#  roadrunner.noticesOff()
+#  for i in range (0:20):
+#      result = r.simulate (0, 10)
+#      r.plot (result)
+#      r.model.k1 = r.model.k1 + 0.2
+#  # Turn the notices back on
+#  roadrunner.noticesOn()
+#@endcode
 
 ##\defgroup filefunctions  File Help Functions
 # \brief Save and Read file methods
+#
+# Use these routines to save or read text files to and from disk
+#
+#Examples:<br>
+#@code
+#
+# r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+# saveToFile ('mymodel.m', r.getMatlab())
+#
+# sbmlstr = readFromFile ('mymodel.xml')
+#@endcode
 
 ##\defgroup loadingModels  Loading Models
 # \brief Methods to load models in different formats
+#
+# There are a variety of methods to load models into libRoadrunner. At the most basic level one can load the model directly using
+# libRoadRunner:
+#@code
+#  r = roadrunner.RoadRunner ('mymodel.xml')
+#@endcode
+#
+#Alternatively one can use these methods:
+#@code
+#  # This is the same as roadrunner.RoadRunner() but the method name is more suggestive of what it does
+#  # Like RoadRunner, loadSBML can accept a file name or a SBML string as it argument
+#
+#  r = te.loadSBMLModel ('mymodel.xml')
+#
+#  # To load an Antimony model use:
+#  r = te.loadAntimonyModel (antStr)
+#
+#  # Or alternatively
+#  r = te.loadAntimonyModel ('mymodel.ant')
+#
+#  # The method loada is simply a shortcut to loadAntimonyModel
+#  r = loada ('''
+#      S1 -> S2; k1*S1;
+#      S2 -> S3; k2*S2;
+#      
+#      k1= 0.1; k2 = 0.2; 
+#      S1 = 10; S2 = 0; S3 = 0;
+#  '''
+#  result = r.simulate (0, 10, 100)
+#  r.plot (result)
+#@endcode
+
 
 ##\defgroup interconversion  Interconversion Methods
 # \brief Methods to interconvert different formats
+#
+# Use these routines interconvert verious standard formats
+#
+#Examples:<br>
+#@code
+#  # Convert an SBML model into Antimony
+#
+#  # Load an SBML file
+#  sbmlStr = te.readFromFile ('mymodel.xml')
+#  # Generate the Antimony format of the SBML model
+#  print te.sbmlToAntimony (sbmlStr)
+#@endcode
+#<br>
+#@code
+#  # Convert an Antimony model into SBML
+#
+#  # Load an Antimony file
+#  antStr = te.readFromFile ('mymodel.ant')
+#  # Generate the SBML format of the Antimony model
+#  print te.antimonyToSBML (antStr)
+#@endcode
 
 ##\defgroup stochastic  Stochastic Simulation Methods
 # \brief Methods to carry out stochastic simulations
+#
+# Use these routines to carry out Gillespie style stochastic simulations
+#
+#Examples:<br>
+#@code
+#
+#  r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 40')
+#  r.setSeed (87675)
+#  result = r.gillespie (0, 100)
+#  r.plot (result)
+#@endcode
+#
+# Run two simulations and combine the two:
+#
+#@code{.py}
+#  import numpy as np
+#  import tellurium as te
+#
+#  r = te.loadSBMLModel ('mymodel.xml')
+#  seed= r.getSeed()
+#  result1 = r.gillespie (0, 100)
+#  r.model.k1 = r.model.k1*20
+#  result2 = r.gillespie (100, 200)
+#  # Merge the two runs together
+#  rr.plot (np.vstack ((result1, result))
+#@endcode
 
 ##\defgroup math  Math Utilities
 # \brief Useful math utilities
+#
+# Only one routine is currently available in this group which is a routine to compute the eigenvalues of given a matrix.
+#
+#Examples:<br>
+#@code
+#
+#import numpy as np
+#import tellurium as te
+#
+#m = np.matrix ([[1,2],[5,7]])
+#
+#print te.getEigenvalues (m)
+#[-0.35889894  8.35889894]
+#@endcode
 
 ##\defgroup plotting  Plotting Utilities
 # \brief Useful plotting utilities
+#
+# Two useful plotting routines. They assume that the first column in the array is the x-axis and the second and subsequent columns represent curves on the y-axis.
+#
+#Examples:<br>
+#@code
+#
+#  # Load a model and carry out a simulation generating 100 points
+#  r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+#  result = r.simulate (0, 10, 100)
+#
+#  # No legend will be add to the plot, useful for plotting large 
+#  # numbers of curves where a legend would get in the way
+#  te.plotArray (result)
+#
+#  # To get a legend use the roadrunner plot command
+#  r.plot (result)
+#@endcode
 
 ##\defgroup resetmethods  Model Reset Methods
 # \brief Model reset methods
+#
+# Use these routines reset your model back to particular states
+#
+#Examples:<br>
+#@code
+#  r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+#  result = r.simulate (0, 10, 100)
+#  p.model.S1 = 2.0
+#  result = r.simulate (0, 10, 100)
+#  # Reset the model back to its original state
+#  r.reset()
+#@endcode
+#
+# If you wish to reset a model back to the state it was what it was loaded, use the resetToOrigin method
+#
+#@code
+#  r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+#  result = r.simulate (0, 10, 100)
+#  # Make lots of different kinds of changes to the model
+#  # Reset the model back to the state it had when it was created
+#  r.resetToOrigin()
+#@endcode
 
 ##\defgroup export Matlab Export Utilities
-# \brief Matlab export utilities
+##\brief Matlab export utilities
+#
+# Use these routines to convert your model into a Matlab function.
+#
+#Examples:<br>
+#@code
+#
+# r = te.loada ('S1 -> S2; k1*S1; k1 = 0.1; S1 = 10')
+# print r.getMatlab()
+# r.exportToMatlab ("mymodel.m")
+#@endcode
 
 ##\defgroup jarnac Short-cut methods
 # \brief Useful short-cut methods
 
 ##\defgroup testmodels Test Models
 # \brief Methods to acess the builtin test models
-
+#
+# RoadRunner has built into it a number of predefined models that can be use
+# to easily try out roadrunner if so exmaple you don't have a model at hand.
+#
+#Examples:<br>
+#@code
+#  # To get the number of builtin models use lisTestModels
+#  print roadrunner.listTestModels()
+#  ['feedback.xml', 'test_1.xml']
+#@endcode
+#
+# To load one of the test models use loadTestModel:
+#@code
+# r = roadrunner.loadTestModel ('feedback.xml')
+# result = r.simulate (0, 10, 100)
+# r.plot (result)
+#@endcode
+#
+# If you need to obtain the SBML for the test model, use getTestModel
+#
+#@code
+#  sbmlStr = roadrunner.getTestModel
+#  saveToFile ('model.xml', sbmlStr)
+#@endcode
+#
+#To look at one of the test model in Antimony form:
+#
+#@code
+#  antstr = te.sbmlToAntimony (roadrunner.getTestModel ('feedback.xml'))
+#  print antStr
+#@endcode
 
 # OLD CODE----------------------------------------
 #def augmentRoadrunnerCtor():
