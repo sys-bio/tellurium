aac19be0fefeea9eb3ea150995204d3d410da94f
Matthias Koenig <konigmatt@googlemail.com>
2016-01-18 19:10:59 +0100

    Parameter slider fixed

--- tellurium/widgets/_parameter_slider.py
+++ tellurium/widgets/_parameter_slider.py
@@ -1,16 +1,22 @@
-from IPython.html.widgets import interact
-from IPython.html import widgets
+"""
+IPython parameter slider.
+"""
+from __future__ import print_function, division
 import sys
+import ipywidgets
+import warnings
+import roadrunner
+from roadrunner import SelectionRecord
 
-def simulateAndPlot(r, start, end, steps, selection=None):
+
+def simulateAndPlot(r, *args, **kwargs):
+    """ Simulate with r.simulate with given arguments and plot with tellurium. """
+    result = r.simulate(*args, **kwargs)
     import tellurium as te
-    if selection is None:
-        result = r.simulate(start, end, steps)
-    else:
-        result = r.simulate(start, end, steps, selection)
     te.plotWithLegend(r, result)
 
-class ParameterSlider():
+
+class ParameterSlider:
     """
     Create interactive sliders to change model parameters.
 
@@ -23,25 +29,28 @@ class ParameterSlider():
                            to determine maximum value of slider
     sliderStepFactor (optional) - scale factor divided with parameter value,
                                   to determine step size of slider
+    selection - roadrunner selection, if None r.selections is used
+    simulateAndPlot - simulateAndPlot function with arguments r, *args, **kwargs
 
     Example Usage:
 
-    import tellurium as te
-    from biomodeltoolbox.widgets import ParameterSlider
-    model = '''
-      model pathway()
-        S1 -> S2; k1*S1 - k2*S2 # Reversible term added here
+        %matplotlib inline
+        import tellurium as te
+        from tellurium.widgets import ParameterSlider
 
-        # Initialize values
-        S1 = 5; S2 = 0;
-        k1 = 0.1;  k2 = 0.05;
+        model = '''
+              model pathway()
+                S1 -> S2; k1*S1 - k2*S2 # Reversible term added here
 
-      end
-    '''
-    r = te.loadAntimonyModel(model)
-    ParameterSlider(r, paramIds=['k1'])
+                # Initialize values
+                S1 = 5; S2 = 0;
+                k1 = 0.1;  k2 = 0.05;
+
+              end
+        '''
+        r = te.loadAntimonyModel(model)
+        ParameterSlider(r, paramIds=['k1'])
     """
-
 
     def __init__(self, r,
                  paramIds=None,
@@ -54,42 +63,49 @@ class ParameterSlider():
 
         if paramIds is None:
             paramIds = r.model.getGlobalParameterIds()
-        paramMap = {}
+        if selection is not None:
+            r.selections = selection
 
-        def runSim(start=0, stop=100, steps=100, **paramMap):
-            r.reset()
-            for k, v in paramMap.items():
+        kwargs = {}
+
+        def runSimulation(start=0, stop=100, steps=100, **kwargs):
+            # full model reset
+            r.reset(SelectionRecord.ALL)
+            # set parameters, key:value pairs
+            for k, v in kwargs.items():
                 try:
                     key = k.encode('ascii', 'ignore')
                     r[key] = v
-                except:
-                    # error in setting model variable
+                except RuntimeError:
+                    # error in setting model variable, variable probably not in model
                     e = sys.exc_info()
-                    print e
-
+                    warnings.warn(e)
+            # simulate
             try:
-                simulateAndPlot(r, start, stop, steps, selection)
+                simulateAndPlot(r, start, stop, steps)
             except:
                 # error in simulation
                 e = sys.exc_info()
-                print e
+                warnings.warn(e)
 
-        for i, id in enumerate(paramIds):
-            val = r[id]
+        # create FloatSlider for all parameters
+        for i, pid in enumerate(paramIds):
+            val = r[pid]
             try:
-                r[id] = val
-                paramMap[id] = widgets.FloatSliderWidget(
+                r[pid] = val
+                kwargs[pid] = ipywidgets.FloatSlider(
                     min=minFactor*val,
                     max=maxFactor*val,
                     step=val/sliderStepFactor,
                     value=val)
             except:
                 e = sys.exc_info()
-                print e
+                print(e)
 
-        interact(runSim,
-                 start=widgets.FloatTextWidget(min=0, value=0),
-                 stop=widgets.FloatTextWidget(min=0, value=100),
-                 steps=widgets.IntTextWidget(min=0, value=100),
-                 **paramMap
-                 )
+        # create the widget
+        ipywidgets.interact(runSimulation,
+                            start=ipywidgets.FloatText(min=0, value=0),
+                            stop=ipywidgets.FloatText(min=0, value=100),
+                            steps=ipywidgets.IntText(min=0, value=100),
+                            **kwargs
+                            )
