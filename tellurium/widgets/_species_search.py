8f9f07add2cac79c5ea74c75da63533edbb5f917
Matthias Koenig <konigmatt@googlemail.com>
2016-01-26 10:13:50 +0100

    notebooks updated

--- tellurium/widgets/_species_search.py
+++ tellurium/widgets/_species_search.py
@@ -1,13 +1,18 @@
 """
-Form for searching by species.
+Form for searching BioModels by species information.
+Uses the bioservices WebServices to retrieve information.
 
-Textboxes can be formated via
-    output_box = widgets.Textarea()
-    output_box.height = '400px'
-    output_box.font_family = 'monospace'
-    output_box.color = '#AAAAAA'
-    output_box.background_color = 'black'
-    output_box.width = '800px'
+Search via bioservices is done via
+::
+
+    import bioservices
+    s = bioservices.BioModels()
+    ch = bioservices.ChEBI()
+    results = ch.getLiteEntity("D-glucose 6-sulfate")[0:5]
+    print(results)
+    print(type(results[0]))
+    print(results[0].chebiId)
+
 """
 from __future__ import print_function, division
 
@@ -17,55 +22,69 @@ import bioservices
 
 
 class SearchBySpeciesForm(object):
-    def __init__(self):
+    """ ipywidgets form for searching biomodels by species."""
+
+    def __init__(self, debug=False):
+        """ Creates and displays the search form. """
+        self.debug = debug
+
         self.s = bioservices.BioModels()
         self.ch = bioservices.ChEBI()
 
-        self.widgets = {
-            'searchTerm': w.Text(description='Search biomodels by species:'),
-            'searchButton': w.Button(description='Search'),
-            'selectChebis': w.Select(description='Matching ChEBI:'),
-            'selectModels': w.Select(description='Matching BioModels:'),
-            'selectedModel': w.FlexBox(children=[
-                w.Textarea(description='Model ID:'),
-                w.Textarea(description='Install Code:'),
-                w.Textarea(description='Import module code:'),
-                w.Textarea(description='Model SBML:')
-
-            ])
-        }
-        self.container = w.FlexBox(children=[
-            self.widgets['searchTerm'],
-            self.widgets['searchButton'],
-            self.widgets['selectChebis'],
-            self.widgets['selectModels'],
-            self.widgets['selectedModel']
+        # Define widgets
+        # <Search>
+        # w.HTML # TODO: HTML display
+        self.wSearchTerm = w.Text(description='Search biomodels by species:', value="CHEBI:17925")
+        self.wSearchButton = w.Button(description='Search')
+        self.wSearchButton.on_click(self.searchChebi)
+        self.wSearchChebi = w.HBox(children=[
+            self.wSearchTerm, self.wSearchButton
         ])
-        # functionality of widgets
 
-        self.widgets['searchButton']
-        self.widgets['searchButton'].on_click(self.search)
-        self.widgets['selectChebis'].on_trait_change(self.selectChebi)
-        self.widgets['selectModels'].on_trait_change(self.selectedModel)
+        self.wSelectChebis = w.Select(description='Matching ChEBI:', width='600px', height='250px')
+        self.wSelectChebis.on_trait_change(self.selectChebi)
+        self.wSelectModels = w.Select(description='Matching BioModels:', width='200px')
+        self.wSelectModels.on_trait_change(self.selectedModel)
 
-        display(self.container)
-        self.init_display()
+        # <Model>
+        self.wModelId = w.Text(description='Model ID:', value="No model selected")
+        self.wModelCode = w.Text(description='Install Code:')
+        self.wModelImport = w.Text(description='Import module code:')
+        self.wModelSbml = w.Textarea(description='Model SBML:', width='800px', height='300px')
 
-    def init_display(self):
-        """ Display the search form. """
+        # <Container>
+        self.wModel = w.FlexBox(children=[
+            self.wModelId,
+            self.wModelCode,
+            self.wSelectModels,
+            self.wModelImport,
+            self.wModelSbml
+        ])
+        for ch in self.wModel.children:
+            ch.font_family = 'monospace'
+            ch.color = '#AAAAAA'
+            ch.background_color = 'black'
+
+        self.wContainer = w.FlexBox(children=[
+            self.wSearchChebi,
+            self.wSelectChebis,
+            self.wModel
+        ])
+
+        # display the widgets
+        display(self.wContainer)
+        # clear notebook output
         clear_output()
-        self.widgets["searchTerm"].value = "D-glucose"
-        for key, widg in self.widgets.iteritems():
-            widg.visible = True
 
-    def search(self, b):
-        """ Search Chebi.
+    def searchChebi(self, b):
+        """ Search Chebi with search term and updates the Chebi selection based on results.
 
         :param b: ?
         :type b: ?
         """
-        term = self.widgets['searchTerm'].value
-        print("searchTerm:", term)
+        term = self.wSearchTerm.value
+        if self.debug:
+            print("searchTerm:", term)
         results = self.ch.getLiteEntity(term)
         choices = [res.chebiId for res in results]
         choiceText = ['%s (%s)' % (res.chebiId, res.chebiAsciiName) for res in results]
@@ -73,30 +92,44 @@ class SearchBySpeciesForm(object):
         options = {}
         for choice, text in zip(choices, choiceText):
             options[text] = choice
-        self.widgets['selectChebis'].options = options
+        self.wSelectChebis.options = options
 
     def selectChebi(self, trait):
         """ Action happening on selection of Chebi Term.
         Search of corresponding BioModels.
-        :param trait: Chebi term
+        :param trait: ?
         :type trait: str
         """
         if trait == 'value':
-            chebi = self.widgets['selectChebis'].value
-            print("selected Chebi:", chebi)
+            chebi = self.wSelectChebis.value
+            if self.debug:
+                print("selected Chebi:", chebi)
             modelIds = self.s.getModelsIdByChEBIId(chebi)
             options = {}
             if modelIds is not None:
                 for mid in modelIds:
                     options[mid] = mid
-            self.widgets['selectModels'].options = options
+            self.wSelectModels.options = options
 
     def selectedModel(self, trait):
+        """ Action happening on selection of Model.
+        Update of model information.
+        :param trait: ?
+        :type trait: str
+        """
         if trait == 'value':
-            modelId = self.widgets['selectModels'].value
-            print("selected Model:", modelId)
+            modelId = self.wSelectModels.value
+            if self.debug:
+                print("selected Model:", modelId)
             sbml = self.s.getModelById(modelId)
-            self.widgets['selectedModel'].children[0].value = modelId
-            self.widgets['selectedModel'].children[1].value = 'pip install git+https://github.com/biomodels/%s.git' % modelId
-            self.widgets['selectedModel'].children[2].value = 'import %s' % modelId
-            self.widgets['selectedModel'].children[3].value = sbml
+            self.wModelId.value = modelId
+            self.wModelCode = 'pip install git+https://github.com/biomodels/%s.git' % modelId
+            self.wModelImport.value = 'import %s' % modelId
+            self.wModelSbml.value = sbml
+
+    def getSBML(self):
+        """ Get the SBML string of the current search.
+        :return: SBML string
+        :rtype: str
+        """
+        return str(self.wModelSbml.value)
