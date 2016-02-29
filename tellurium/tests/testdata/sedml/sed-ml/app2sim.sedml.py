c03120c0af080ff5de2fe279bfbfb78425a059cd
Matthias Koenig <konigmatt@googlemail.com>
2016-02-29 08:13:25 +0100

    updated sed-ml examples

--- tellurium/tests/testdata/sedml/sed-ml/app2sim.sedml.py
+++ tellurium/tests/testdata/sedml/sed-ml/app2sim.sedml.py
@@ -1,7 +1,7 @@
 """
     tellurium 1.3.1
 
-    auto-generated code (2016-02-26T19:54:38)
+    auto-generated code (2016-02-29T08:07:17)
     sedmlDoc: L1V1  
     workingDir: /home/mkoenig/git/tellurium/tellurium/tests/testdata/sedml/sed-ml
     inputType: SEDML_FILE
