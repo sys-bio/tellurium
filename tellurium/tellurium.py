ca7e423643f34d85746cbf1556fb9f0ea75b2362
Herbert Sauro <hsauro@u.washington.edu>
2017-02-21 09:50:05 -0800

    Update tellurium.py
    Added comment strings to runTool

--- tellurium/tellurium.py
+++ tellurium/tellurium.py
@@ -161,17 +161,17 @@ def readFromFile(filePath):
 
 
 def runTool (toolFileName):
-    # """
-	# Call an external application called toolFileName
-	# NOte that .exe extension may be omitted for windows applications.
+     """
+     Call an external application called toolFileName
+     Note that .exe extension may be omitted for windows applications.
 	
-	# Include any arguments in arguments parameter.
+     Include any arguments in arguments parameter.
 	
-	# Example:
-	# returnString = te.runTool (['myplugin', 'arg1', 'arg2'])
-	#
-	# If the external tool writes to stdout, this will be captured by this call.
-	# """
+     Example:
+        returnString = te.runTool (['myplugin', 'arg1', 'arg2'])
+
+     If the external tool writes to stdout, this will be captured by this call.
+     """
     try:
       if (os.name == 'nt') and (not toolFileName[0].endswith ('.exe')):
          toolFileName[0] = toolFileName[0] + '.exe'
