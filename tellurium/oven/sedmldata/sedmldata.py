08c37984d63f409f4fa46996d4452cb5b1973d4f
Matthias Koenig <konigmatt@googlemail.com>
2017-06-28 15:20:33 -0700

    work on data support

--- tellurium/oven/sedmldata/sedmldata.py
+++ tellurium/oven/sedmldata/sedmldata.py
@@ -19,26 +19,78 @@ except ImportError:
 print('Version:', libsedml.getLibSEDMLDottedVersion())
 
 
-def load_csv_data(source):
-    """ Helper function for loading data file from given source.
-    :param source: source information from the SED-ML file
-    :return: data matrix
+SOURCE_CSV = "./oscli.csv"
+SOURCE_NUML = "./oscli.numl"
+
+
+def parse_dimension_description(dd):
+    """ Parses the dimension information from the dimension description.
+
+          <dimensionDescription>
+            <compositeDescription indexType="double" name="time">
+              <compositeDescription indexType="string" name="SpeciesIds">
+                <atomicDescription valueType="double" name="Concentrations" />
+              </compositeDescription>
+            </compositeDescription>
+          </dimensionDescription>
+          <dimension>
+
+    :param dd:
+    :return:
     """
-    raise NotImplemented
+    cd_top = dd.getComposiiteDescription()
+    print(cd_top)
 
 
 def load_numl_data(source):
     """ Helper function for loading data files from given source.
+
+    For more information see:
+        https://github.com/numl/numl
+
     :param source: 
     :return: data matrix
     """
-    raise NotImplemented
+    # FIXME: handle urn/url sources
+
+    doc_numl = libsedml.readNUMLFromFile('./oscil.numl')
+    print(doc_numl)
+
+    # reads all the resultComponents from the numl file
+
+    dd = doc_numl.getDimensionDescription()
+
+    # TODO: figure out the dimensionality, and create np.array
+    # if 2D return a DataFrame (Does this work with the slicing?)
+
+    columns = None
+    data = None
+
+    df = None
+    return df
 
 
+def load_csv_data(source):
+    """ Helper function for loading data file from given source.
 
-# load numl document
-doc_numl = libsedml.readNUMLFromFile('./oscil.numl')
-print(doc_numl)
+    CSV files must have a header.
+
+    :param source: source information from the SED-ML file
+    :return: data matrix
+    """
+    # FIXME: handle urn/url sources
+    # FIXME: figure out the separator for data reading based on the dimension
+    sep = ","
+    df = pd.read_csv(source, sep=sep)
+    return df
+
+
+# load data from sources
+df_csv = load_csv_data(SOURCE_CSV)
+print(df_csv.head())
+
+df_numl = load_csv_data(SOURCE_NUML)
+print(df_numl)
 
 
 # --------------------------------------
