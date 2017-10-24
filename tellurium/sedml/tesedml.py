70f035d5f445b8541fb28ab663c53f422bf926c2
0u812 <0u812@users.noreply.github.com>
2017-10-24 13:25:28 -0700

    revert process_trace

--- tellurium/sedml/tesedml.py
+++ tellurium/sedml/tesedml.py
@@ -1912,15 +1912,15 @@ def process_trace(trace):
     if trace.size > 1:
         # FIXME: this adds a nan at the end of the data. This is a bug.
         if len(trace.shape) == 1:
-            # return np.concatenate((np.atleast_1d(trace), np.atleast_1d(np.nan)))
-            return np.atleast_1d(trace)
+            return np.concatenate((np.atleast_1d(trace), np.atleast_1d(np.nan)))
+            #return np.atleast_1d(trace)
 
         elif len(trace.shape) == 2:
             #print('2d trace')
             # print(trace.shape)
             # FIXME: this adds a nan at the end of the data. This is a bug.
-            # result = np.vstack((np.atleast_1d(trace), np.full((1,trace.shape[-1]),np.nan)))
-            result = np.vstack((np.atleast_1d(trace), np.full((1, trace.shape[-1]))))
+            result = np.vstack((np.atleast_1d(trace), np.full((1,trace.shape[-1]),np.nan)))
+            #result = np.vstack((np.atleast_1d(trace), np.full((1, trace.shape[-1]))))
             return result
     else:
         return np.atleast_1d(trace)
