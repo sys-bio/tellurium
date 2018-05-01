0c4d3f3f9e04f5106b3a40d1e50f5e00f398f20d
0u812 <0u812@users.noreply.github.com>
2018-05-01 15:57:21 -0700

    add support for annotating functions via SBO terms, fix #340

--- tellurium/teconverters/antimony_regex.py
+++ tellurium/teconverters/antimony_regex.py
@@ -6,12 +6,16 @@ def getModelStartRegex():
 
 def getFunctionStartRegex():
     """ Return the regex string for Antimony model start. Matches whole line. """
-    return r'^\s*function\s*[^()\s]*\s*(\([^)]*\))?\s*$'
+    return r'^\s*function\s*([^()]*)\s*(\([^)]*\))?\s*$'
 
 def getModelEndRegex():
     """ Return the regex string for Antimony model end. Matches whole line. """
     return r'^\s*end\s*$'
 
 def getSBORegex():
-    """ Return the regex string for Antimony model end. Matches whole line. """
-    return r'^\s*([^.]+)\.sboTerm\s*=\s*(SBO:)?([0-9]+)\s*(;)?\s*$'
+    """ Return the regex string for an SBO term. Matches whole line. """
+    return r'^\s*([^.\s]+)\.sboTerm\s*=\s*(SBO:)?([0-9]+)\s*(;)?\s*$'
+
+def getFunctionSBORegex():
+    """ Return the regex string for a function SBO term. Matches whole line. """
+    return r'^\s*sboTerm\s*=\s*(SBO:)?([0-9]+)\s*(;)?\s*$'
