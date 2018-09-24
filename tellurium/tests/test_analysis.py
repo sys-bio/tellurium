e2b69db7b8478473ed20896020e13df144c784ae
Kiri Choi <kirichoi@uw.edu>
2018-09-24 16:32:46 -0700

    Fix test

--- tellurium/tests/test_analysis.py
+++ tellurium/tests/test_analysis.py
@@ -15,7 +15,7 @@ class MyTestCase(unittest.TestCase):
     def test_plot2DParameterScan(self):
         """Test plot2DParameterScan."""
         import tellurium as te
-        from tellurium.analysis.parameterscan import plot2DParameterScan
+        from tellurium.analysis import ParameterScan2D
         r = te.loada("""
         model test
            J0: S1 -> S2; Vmax * (S1/(Km+S1))
@@ -27,10 +27,11 @@ class MyTestCase(unittest.TestCase):
         # r.plot(s)
 
         import numpy as np
-        plot2DParameterScan(r,
+        p = ParameterScan2D(r,
                             p1='Vmax', p1Range=np.linspace(1, 10, num=5),
                             p2='Vmax', p2Range=np.linspace(0.1, 1.0, num=5),
                             start=0, end=50, points=101)
+        p.plot2DParameterScan()    
 
 
 if __name__ == '__main__':
